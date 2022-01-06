0849773b18589afcb7018d04f999bc90078b61c2
ARNE12 <leatherg21@gmail.com>
2022-01-06 14:24:49 -0800

    worked on the intro challenges

---
+++ ruby-intro-regina-warren.rb
@@ -0,0 +1,19 @@
+my_favnum = 3 
+p my_favnum / 2
+p my_favnum / 2.00
+
+warren_num = 33
+warren_num = 44
+p warren_num
+
+p my_favnum - warren_num
+
+# my_favnum * 26!
+p my_favnum * 26
+
+favday = 'friday'
+p "my favorite day is 
+p "#{favday}"
+
+
+
