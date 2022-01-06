24a17e9e1e91d225cfa0eae8168abe7a36b29f58
mikkajimenez <mikka.jimenez@gmail.com>
2022-01-06 11:21:05 -0800

    arithmetic and variable parts done

---
+++ intro-hannah-raul-mikka.rb
@@ -0,0 +1,18 @@
+puts 4+4
+puts 5-4
+puts 5*4.01
+puts 5.12/4
+puts 7%3
+# puts 7/0
+# puts 0/0
+
+puts my_favorite_number = 21
+puts my_favorite_number / 2
+
+puts someones_favorite = 13
+puts someones_favorite = 7
+puts my_favorite_number - someones_favorite
+
+puts my_favorite_number = my_favorite_number * 26
+puts my_favorite_number
+
