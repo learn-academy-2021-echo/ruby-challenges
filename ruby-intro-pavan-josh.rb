426e1f8bb1602121e55234d2a715aab2d52311da
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2022-01-06 11:35:17 -0800

    we learned put

---
+++ ruby-intro-pavan-josh.rb
@@ -0,0 +1,6 @@
+my_favorite_number = 5
+p my_favorite_number/2.0
+p someones_favorite_number=13
+p someones_favorite_number=7
+p my_favorite_number-someones_favorite_number
+p my_favorite_number=my_favorite_number*26
