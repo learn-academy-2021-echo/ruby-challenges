96bf22b9b5e23f1f2da6048dd5f9318d8f26957d
DeonFranklin <deondeefranklin@gmail.com>
2022-01-06 15:39:44 -0800

    Conditionals Challenges

---
+++ condtionals_antonio_deon.rb
@@ -0,0 +1,32 @@
+# Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
+# Story: As a user, I can see a message in the terminal saying if there was a tie.
+
+def RPC
+puts 'Player 1, Pick: "rock", "paper", or "scissors"'
+# puts 'Type "rock", "paper", or "scissors"'
+input1 = gets.chomp
+puts "Player 1, you chose #{input1}!"
+
+puts 'Player 2, Pick: "rock", "paper", or "scissors"'
+input2 = gets.chomp
+puts "Player 2, you chose #{input2}!"
+
+puts "Player 1 chose #{input1} and Player 2 chose #{input2}!"
+
+def rock_paper_scissors(input1, input2)
+    if input1 == input2
+    p 'The game was a tie!'
+    end
+    if (input1 == "rock" && input2 == "scissors" || input1 == "paper" && input2 == "rock" || input1 == "scissors" && input2 == "paper")
+    puts "Player 1 wins"
+    end
+    if (input2 == "rock" && input1 == "scissors" || input2 == "paper" && input1 = "rock" || input2 == "scissors" && input1 = "paper")
+    puts "Player 2 wins"
+    end
+
+
+end
+p rock_paper_scissors(input1, input2)
+end
