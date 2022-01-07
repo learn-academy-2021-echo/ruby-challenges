cec6bcc793e64e8f783ddef883eaf0bdb56c389c
Frank Carbonero <frankcarbonero@icloud.com>
2022-01-07 11:31:49 -0800

    blocks-fc-sp

---
+++ blocks-frank-samantha.rb
@@ -0,0 +1,17 @@
+# Write a loop that prints the numbers 1 to 20.
+# Write a loop that prints the numbers 1 to 20 using a different approach than previously used.
+# Write a loop that prints only even numbers from 20 to 0.
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+# Create a method that takes in an array of numbers and returns the smallest number in the array.
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
+# Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
+
+nums = [1-20]
+    nums.each do |value|
+puts value
+    end
+
+
