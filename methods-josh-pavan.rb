f061a0f13b257bc0a713cac539e91d46b0fc3d93
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2022-01-06 16:36:04 -0800

    We did the intro challanges.

---
+++ methods-josh-pavan.rb
@@ -0,0 +1,25 @@
+# def  sum_these_numbers (num1,num2)
+# num1+num2
+# end
+# p sum_these_numbers(6,8)
+
+# def is_even (num1)
+# if num1 % 2 == 0
+#  true
+# else
+#  false
+# end
+# end
+#   p is_even (9)
+# def less_than_ten(num1)
+#   if num1 >10
+#     'invalid'
+#   else
+#     'valid'
+# end
+# end
+# p less_than_ten (8)
+def is_palindrome (string)
+  string==string.reverse
+end
+p is_palindrome ('pavan')
