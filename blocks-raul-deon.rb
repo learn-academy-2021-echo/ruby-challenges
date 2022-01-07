58319e66c00cd7e0831de267578dd455f47511a7
Echo5Mike <1raul.marquez@gmail.com>
2022-01-07 11:56:45 -0800

    completed the first 3 questions

---
+++ blocks-raul-deon.rb
@@ -0,0 +1,57 @@
+# Write a loop that prints the numbers 1 to 20.
+
+nums = 1..20
+# nums.map do |value|
+#     p value
+# end
+
+# Write a loop that prints the numbers 1 to 20 using a different approach than previously used.
+# p nums.to_a
+
+
+
+# Write a loop that prints only even numbers from 20 to 0.
+i = 20
+# while i >= 0
+#     puts i 
+#     i -= 2
+# end
+
+
+
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+nums_array = [1,2,7,18]
+# nums_array.each do |value|
+#     p value * 5
+# end
+
+
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+new_array = ['raul', 'deon', 'learn']
+puts new_array.to_s.upcase
+puts new_array
+# puts new_array.upcase
+
+
+
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+
+
+
+
+# Create a method that takes in an array of numbers and returns the smallest number in the array.
+
+
+
+
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+
+
+
+
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
+
+
+
+
+# Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
