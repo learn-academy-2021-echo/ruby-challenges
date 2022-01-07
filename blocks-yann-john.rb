ac6d5ba3b2c2acb933ce0aaf1a42dfcb5b9b4103
yannsf29 <yann.crennsf@gmail.com>
2022-01-07 11:58:57 -0800

    block

--- blocks-yann-john.rb
+++ blocks-yann-john.rb
@@ -35,11 +35,62 @@
 
 # Create a method that takes in an array of numbers and returns the largest number in the array.
 
+# num_array = [5, 10, 20, 30 , 4]
+# def maxnumber value 
+# value.max
+# end
+
+# p maxnumber num_array
+
 
 # Create a method that takes in an array of numbers and returns the smallest number in the array.
 
+# num_array = [5, 10, 20, 30 , 4]
+# def minnumber value 
+# value.min
+# end
+
+# p minnumber num_array
+
+
+
 # Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+
+# nums = [ 2, 3, 4 , 7, 6, 5, 8, 9, 10]
+
+# def is_odd array
+#     array.select do |value|
+#         value.odd?
+#     end    
+
+# end
+
+# p (is_odd nums).sort
 
 # Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
 
+# name = "EcHoIsThEbEsT"
+
+
+
+
+
+
+
+
+
 # Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
+
+
+# start_array = ['Apple', 'Avocado', 'Banana', 'Mango']
+
+# def string_array(array, string)
+#     array.select do |value|
+#     value.include? string.upcase  
+#     end
+# end
+
+# p string_array(start_array, 'a')
+    
+
+    
