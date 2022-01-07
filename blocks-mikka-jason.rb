d1f94567e7fd586b16a54e2f25abd4c43707ca3d
mikkajimenez <mikka.jimenez@gmail.com>
2022-01-07 14:00:52 -0800

    blocks challenges done

--- blocks-mikka-jason.rb
+++ blocks-mikka-jason.rb
@@ -13,7 +13,6 @@
 
 # Write a loop that prints only even numbers from 20 to 0.
 
-
 # nums = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
 # def is_even array
 #     array.select do |value|
@@ -32,7 +31,8 @@
 # p result
 
 # Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
-# array = ["dog","cat","fish"]  
+
+# array = ["dog","cat","fish"]
 #  array.map! do |v|
 #     v.capitalize
 # end
@@ -58,30 +58,44 @@
 #     p is_odd num
 # Create a method that takes in an array of strings and returns all the strings in reverse casing.
 #        All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
-# array = ["Dog", "Cat", "fish","Computer"] 
+
+# array = ["Dog", "Cat", "fish","Computer"]
 # reverseArr = array.map do |v|
 #     v.swapcase
 # end
 # p reverseArr
 
-
-# Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) 
-#        Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. 
+# Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex)
+#        Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado'].
 #        With the same method, given the letter 'm' should return ['Mango'].
-array = ['Apple', 'Avocado', 'Banana', 'Mango']
+# array = %w[Apple Avocado Banana Mango]
 
-def first_letter (array)
-        array.select do |value|
-            value.start_with? 'A'
-        end
-    
-end
+# def first_letter(array)
+#   array.select { |value| value.start_with? 'A' }
+# end
 
-p first_letter array
-
-
+# p first_letter array
 
 # FIZZBUZZ
-# Write a method that prints the numbers from 1 to 100. For multiples of three print Fizz instead of the number, 
-#       for multiples of five print Buzz instead of the number, for numbers that are multiples of both three and five print FizzBuzz, 
-#       for all other numbers print the number.
+
+# Write a method that prints the numbers from 1 to 100. For multiples of three print Fizz instead of the number,
+#for multiples of five print Buzz instead of the number, for numbers that are multiples of both three and five print FizzBuzz,
+#for all other numbers print the number.
+
+range = [*1..100]
+
+def fizzbuzz(range)
+  range.each do |value|
+    if value % 3 == 0 && value % 5 == 0
+      p 'FIZZBUZZ'
+    elsif value % 5 == 0
+      p 'BUZZ'
+    elsif value % 3 == 0
+      p 'FIZZ'
+    else
+      p value
+    end
+  end
+end
+
+fizzbuzz(range)
