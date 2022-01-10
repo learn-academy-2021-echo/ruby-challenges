b0a715da53a03a7b281eb55f7c88bac373985c4b
gluay88 <p.wantagaew@gmail.com>
2022-01-10 10:52:04 -0800

    title done

---
+++ textbased-game-kirk-yann-gluay.rb
@@ -0,0 +1,32 @@
+puts "-THE"
+puts " ****     ****     **     ********** *******   ** **     **"
+puts "/**/**   **/**    ****   /////**/// /**////** /**//**   ** "
+puts "/**//** ** /**   **//**      /**    /**   /** /** //** **  "
+puts "/** //***  /**  **  //**     /**    /*******  /**  //***   "
+puts "/**  //*   /** **********    /**    /**///**  /**   **/**  "
+puts "/**   /    /**/**//////**    /**    /**  //** /**  ** //** "
+puts "/**        /**/**     /**    /**    /**   //**/** **   //**"
+puts "//         // //      //     //     //     // // //     //"
+sleep(3)
+
+puts "You are in a dead end job and know something about this reality is weird,"
+sleep(3)
+
+puts "You are passed out at the desk and you wake up to your computer screen."
+sleep(3)
+
+puts "You look at your computer screen and it says FOLLOW THE WHITE RABBIT"
+sleep(3)
+
+puts "  //  "
+puts " ('>  "
+puts " /rr  "
+puts "*\\))_  "
+
+
+
+
+
+
+
+
