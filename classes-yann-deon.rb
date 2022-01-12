90100bb756304093779412f38231d321b4fce5be
yannsf29 <yann.crennsf@gmail.com>
2022-01-11 16:03:07 -0800

    ruby class

---
+++ classes-yann-deon.rb
@@ -0,0 +1,78 @@
+
+
+# class Task 
+#     def initialize(name, time, is_complete)
+#         @name = name
+#         @time = time
+#         @is_complete = is_complete
+        
+#     end
+
+#     def get_info
+    
+#     if @is_complete == "incompleted"
+#         "your chore is #{@name}, it takes about #{@time}. The status is #{@is_complete}."
+       
+#     elsif @is_complete == "complete"
+#     "your chore was #{@name}, it took you about #{@time}. The status is #{@is_complete}"
+       
+#    end    
+    
+#     end
+# end
+
+
+# dishes = Task.new('dishes', '30min', 'incompleted')
+# p dishes.get_info
+
+# laundry = Task.new('laundry', '45min', 'incompleted')
+# p laundry.get_info
+
+# vacuum = Task.new('vacuum', '35min', 'complete')
+# p vacuum.get_info
+
+
+
+
+
+# For the following ColorPalette challenge use initialize and attr_accessor methods in your class
+# As a developer, I can create a class called ColorPalette.
+# As a developer, I can create three instances (objects) of class ColorPalette. (e.g green = ColorPalette.new)
+# As a developer, I can initialize each inistance of the class ColorPalette with three colors. (e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam'))
+# As a developer, I can print the value of each individual color.
+# As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+# As a developer, I can change one or more colors of a given palette.
+
+
+class ColorPalette
+    
+    def initialize(name, value)
+        @name = name
+        @value = value
+    
+        
+    end
+
+    def get_info
+    
+    if @is_complete == "incompleted"
+        "your chore is #{@name}, it takes about #{@time}. The status is #{@is_complete}."
+       
+    elsif @is_complete == "complete"
+    "your chore was #{@name}, it took you about #{@time}. The status is #{@is_complete}"
+       
+   end    
+    
+    end
+end
+
+
+dishes = Task.new('dishes', '30min', 'incompleted')
+p dishes.get_info
+
+laundry = Task.new('laundry', '45min', 'incompleted')
+p laundry.get_info
+
+vacuum = Task.new('vacuum', '35min', 'complete')
+p vacuum.get_info
+
