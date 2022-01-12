2da2a26955a995d2326007e18701bb1d1b78b82d
DeonFranklin <deondeefranklin@gmail.com>
2022-01-11 16:44:27 -0800

    Today's Challenges

--- classes-yann-deon.rb
+++ classes-yann-deon.rb
@@ -44,35 +44,35 @@
 # As a developer, I can change one or more colors of a given palette.
 
 
-class ColorPalette
-    
-    def initialize(name, value)
-        @name = name
-        @value = value
-    
-        
-    end
+# class ColorPalette
+#     def initialize(color, family)
+#         @color = color
+#         @family = family
+#     end
+#     def get_info
+#         if @color== "color"
+#             "your color is #{@color}, this color belongs to the the #{@color}#{@family}"
+#         end        
+#     end
+# end
 
+
+# green = ColorPalette.new('green', 'giant')
+# p green.get_info
+# # green = ColorPalette.new('jade', 'light', 'warm')
+# # p green.get_info
+
+
+class AnimalKingdom 
+    def initialize(name)
+        @name = name
+        @age = 0
+        @alive = true
+    end
     def get_info
-    
-    if @is_complete == "incompleted"
-        "your chore is #{@name}, it takes about #{@time}. The status is #{@is_complete}."
-       
-    elsif @is_complete == "complete"
-    "your chore was #{@name}, it took you about #{@time}. The status is #{@is_complete}"
-       
-   end    
-    
+        "This animal is #{@alive}, and is #{@age} years old."
     end
 end
 
-
-dishes = Task.new('dishes', '30min', 'incompleted')
-p dishes.get_info
-
-laundry = Task.new('laundry', '45min', 'incompleted')
-p laundry.get_info
-
-vacuum = Task.new('vacuum', '35min', 'complete')
-p vacuum.get_info
-
+lion = AnimalKingdom.new("Simba")
+p lion.get_info
