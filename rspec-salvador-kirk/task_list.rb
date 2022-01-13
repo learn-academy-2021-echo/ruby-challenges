e15c00fd5f0a595f92c242fd0fbfa6abe49339a0
kirkkwang <k3wang@gmail.com>
2022-01-12 21:48:06 -0800

    Finally arrived at the solution, it was .map all along...

--- rspec-salvador-kirk/task_list.rb
+++ rspec-salvador-kirk/task_list.rb
@@ -1,17 +1,25 @@
 require_relative 'task'
 
-
 class TaskList < Task
-    attr_accessor :add_task
-    def initialize
-        @add_task = []
-    end
-    def completed_tasks
-        @add_task.select do |task| 
-            if task.progress == 'completed' 
-                task.task
-            end
-        
-        end
-    end
+  attr_accessor :add_task
+  def initialize
+    @add_task = []
+  end
+  def completed_tasks
+    @add_task
+      .select { |object| object.progress == 'completed' }
+      .map { |object| object.task }
+  end
 end
+
+# my_tasklist = []
+# my_tasklist << Task.new('clean kitchen')
+# my_tasklist[0].complete
+# my_tasklist << Task.new('clean room')
+# my_tasklist[1].complete
+# my_tasklist << Task.new('take out trash')
+# p my_tasklist.length
+
+# p my_tasklist
+#     .select { |object| object.progress == 'completed' }
+#     .map { |object| object.task }
--- rspec-salvador-kirk/task_list_spec.rb
+++ rspec-salvador-kirk/task_list_spec.rb
@@ -3,43 +3,43 @@ require_relative 'task'
 require_relative 'task_list'
 
 describe 'TaskList' do
-      it 'must be real' do
-          #Arrange - none
-          #Action - none
-          #Assert
-          expect {TaskList.new}.to_not raise_error
-      end
-       # Story: As a developer, I can add all of my Tasks to a TaskList.
-    it 'can add all of my Tasks to a Tasklist'  do
-        my_tasklist = TaskList.new
-        my_tasklist.add_task << Task.new
-        #Action
-        #Assert
-        expect(my_tasklist.add_task).to be_a Array
-        expect(my_tasklist.add_task.length).to eq(1)
-    end
-# Story: As a developer with a TaskList, I can print the completed items.
-    it 'can print the completed items'  do
-        my_tasklist = TaskList.new
-        #Action
-        my_tasklist.add_task << Task.new('clean kitchen')
-        my_tasklist.add_task[0].complete
-        my_tasklist.add_task << Task.new('clean room')
-        my_tasklist.add_task[1].complete
-        my_tasklist.add_task << Task.new('take out trash')
+  it 'must be real' do
+    #Arrange - none
+    #Action - none
+    #Assert
+    expect { TaskList.new }.to_not raise_error
+  end
 
-        #Assert
-        expect(my_tasklist.completed_tasks).to be_a Array
-        expect(my_tasklist.completed_tasks).to eq(['clean kitchen', 'clean room'])
-        expect(my_tasklist.completed_tasks.length).to eq(2)
-    end
+  # Story: As a developer, I can add all of my Tasks to a TaskList.
+  it 'can add all of my Tasks to a Tasklist' do
+    my_tasklist = TaskList.new
+    my_tasklist.add_task << Task.new
 
+    #Action
+    #Assert
+    expect(my_tasklist.add_task).to be_a Array
+    expect(my_tasklist.add_task.length).to eq(1)
+  end
+
+  # Story: As a developer with a TaskList, I can print the completed items.
+  it 'can print the completed items' do
+    my_tasklist = TaskList.new
+
+    #Action
+    my_tasklist.add_task << Task.new('clean kitchen')
+    my_tasklist.add_task[0].complete
+    my_tasklist.add_task << Task.new('clean room')
+    my_tasklist.add_task[1].complete
+    my_tasklist.add_task << Task.new('take out trash')
+
+    #Assert
+    expect(my_tasklist.completed_tasks).to be_a Array
+    expect(my_tasklist.completed_tasks.length).to eq(2)
+    expect(my_tasklist.completed_tasks).to eq(['clean kitchen', 'clean room'])
+  end
 end
 
 # Story: As a developer with a TaskList, I can print the incomplete items.
-
-
-
 
 # Stretch: Due Date
 # Story: As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
